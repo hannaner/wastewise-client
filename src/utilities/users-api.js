e24c76baee56809c99733a7ff1fbc692f4b5ef17
Hanna <hanna.kim.x@gmail.com>
2023-03-26 14:19:02 -0700

    remove application type header

--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -18,7 +18,7 @@ export default async function sendRequest(url, method='GET', payload=null){
     
     if (payload){
         options.headers = {
-            'Accept': 'application/json',
+            // 'Accept': 'application/json',
             'Content-type': 'application/json',
         }
         options.body = JSON.stringify(payload)
@@ -30,25 +30,25 @@ export default async function sendRequest(url, method='GET', payload=null){
         options.headers.Authorization = `Token ${token}`
     }
 
-    // const res = await fetch(url, options)
-    // if (res.ok) {
-    //     if (res.status === 204){
-    //         return res
-    //     } else {
-    //         return res.json()
-    //     }
-    // } else {
-    //     console.log(res.error)
-    //     throw new Error('Bad request')
-    // }
     const res = await fetch(url, options)
-    if(method === "DELETE"){
-        return
+    if (res.ok) {
+        if (res.status === 204){
+            return res
+        } else {
+            return res.json()
+        }
+    } else {
+        console.log(res.error)
+        throw new Error('Bad request')
     }
-    else if(res.ok) {
-        return res.json()
-    } 
-    else {
-        throw new Error("Bad Request")
-    }
+    // const res = await fetch(url, options)
+    // if(method === "DELETE"){
+    //     return
+    // }
+    // else if(res.ok) {
+    //     return res.json()
+    // } 
+    // else {
+    //     throw new Error("Bad Request")
+    // }
 }
